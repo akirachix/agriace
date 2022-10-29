18bff308ea2ed0c1497e4bc1c5e401c5cde73ba0
agriace <agriace@gmail.com>
2022-10-29 20:03:33 +0300

    cleared

--- apis/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path,include
-from rest_framework import routers
-from .views import UserViewsets
-
-router = routers.DefaultRouter()
-router.register(r'user',UserViewsets)
-urlpatterns = [
-    path('',include(router.urls)),
-]
