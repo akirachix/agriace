c6a46088cf8e1c0466f977e5e3e06e378d0d06ce
hilda1737 <hildamoraahh@gmail.com>
2022-11-04 06:32:26 +0300

    changes

--- src/Dashboard/Customers.jsx
+++ src/Dashboard/Customers.jsx
@@ -1,57 +1,115 @@
-import '../App.css'
+// import '../App.css'
 
-// const Customers= () => { 
-  import React, { useEffect, useState } from 'react';
-  import axios from 'axios'
+// // const Customers= () => { 
+//   import React, { useEffect, useState } from 'react';
+//   import axios from 'axios'
   
-  function Customers() {
-    const [, setIsLoading] = useState(false);
-    const [, setError] = useState();
-    const [users, setUsers] = useState([]);
-    useEffect(()=>{
-      getUsers()
-      console.log('customers')
-    },[]);
-    const getUsers =() => {
-      console.log('hhhhh')
-        axios.get("https://morning-meadow-48263.herokuapp.com/seed_companies/")
-        .then(res=>{
-          console.log(res.data)
-          setUsers(res.data)
-        })
-    };
-    return(  
+//   function Customers() {
+//     const [, setIsLoading] = useState(false);
+//     const [, setError] = useState();
+//     const [users, setUsers] = useState([]);
+//     useEffect(()=>{
+//       getUsers()
+//       console.log('customers')
+//     },[]);
+//     const getUsers =() => {
+//       console.log('hhhhh')
+//         axios.get("https://morning-meadow-48263.herokuapp.com/seed_companies/")
+//         .then(res=>{
+//           console.log(res.data)
+//           setUsers(res.data)
+//         })
+//     };
+//     return(  
 
+//         <section class="orders">
+//             <div class="orders-list">
+//               <table class="table">
+//                   <thead>
+//                     <th>Companies</th>
+//                     <th>Location</th>
+//                     <th>Seed Variety</th>
+//                     <th>Status</th>
+//                   </thead>
+              
+//              {users.map(item => {
+//           return (
+//             <><tbody /><tr class='active'>
+//               <td>{item.company_name}</td>
+//               <td>{item.location}</td>
+//               <td>{item.seed_variety}</td>
+//               <td><button>Available</button></td>
+
+
+//             </tr><tbody /></>
+            
+//           )
+//         })}
+
+          
+//               </table>
+//             </div>
+
+//           </section>
+        
+//     ) 
+// }
+//  export default Customers;
+import '../App.css'
+import './Sidebar'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+
+      function Customers() {
+        const [, setIsLoading] = useState(false);
+        const [, setError] = useState();
+        const [users, setCustomers] = useState([]);
+        useEffect(()=>{
+          getCustomers()
+          console.log('customers')
+        },[]);
+        const getCustomers=() => {
+          console.log('hhhhh')
+            axios.get( "https://morning-meadow-48263.herokuapp.com/seed_companies/")
+            .then(res=>{
+              console.log(res.data)
+              setCustomers(res.data)
+            })
+        };
+        return(   
         <section class="orders">
             <div class="orders-list">
               <table class="table">
-                <thead class = 'active'>
+                <thead>
                   <tr>
-                    <th>Companies</th>
+                    <th>Company</th>
                     <th>Location</th>
-                    <th>Seed Variety</th>
+                    <th>variety</th>
                     <th>Status</th>
+
                   </tr>
-              
-                </thead >
+
+                </thead>
                 {users.map(item => {
           return (
-            <tr class ='active'>
-              <td>{item.company_name}</td>
-              <td>{item.location}</td>
-              <td>{item.seed_variety}</td>  
-              <td><button>Available</button></td>
-  
-             
-            </tr>
+                <tbody>
+                  <tr class = 'active'>
+                    <td>{item.company_name}</td>
+                    <td>{item.location}</td>
+                    <td>{item.seed_variety}</td>
+                    <td><button>Pending</button></td>
+                  </tr>
+                 
             
-            
-          )
-        })}
+                
+                </tbody>
+)
+})}
 
-          
               </table>
             </div>
+
           </section>
         
     ) 
--- src/Dashboard/Header.jsx
+++ src/Dashboard/Header.jsx
@@ -11,31 +11,18 @@ const Header = props => {
         <h1>Dashboard</h1>
     </div>
 
-    {/* <div class="searchBar">
-        <input type="text" placeholder="Search.."/> */}
-
-        <div className="searchForm" style={props.searchFormStyles}>
-        <input
-          type="text" 
-          className="searchForm__input"
-          placeholder={props.placeholder}
-          value={props.value}
-          onChange={e => {
-            props.onchange(e);
-          }}
-          style={props.searchBoxStyles}
+<form action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Search</span>
+        </label>
+        <input 
+            type="text"
+            id="header-search"
+            placeholder="Search"
+            name="s" 
         />
-         <button
-          type="submit"
-          className="searchForm__btn"
-          style={props.searchButtonStyles}
-          onClick={e => {
-            props.OnSubmit(e);
-          }}
-        >
-          <i className="fa fa-search" style={props.searchIconStyles} />
-        </button>
-    </div>
+        <button type="submit">Search</button>
+    </form>
 
 
 </div>
@@ -44,55 +31,6 @@ const Header = props => {
   )
 }
 export default Header;
-
-
-
-// import '../App.css';
-// // import logo from '../../images/logo.png';
-// import { useRef, useEffect } from 'react';
-// import { AiOutlineSearch } from 'react-icons/ai';
-// import { useGlobalContext } from '../contex/contex'
-
-
-// const Header = () => {
-//   const { searchMeals } = useGlobalContext();
-//   const inputRef = useRef(null);
-
-//   useEffect(() => {
-//     inputRef.current.focus();
-//   }, []);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     searchMeals(inputRef.current.value);
-//   };
-
-//   return (
-//     <div className="header">
-//       {/* <img className="header__logo" src={logo} alt="logo" /> */}
-//       <p>Available Recipes</p>
-
-//       <form className="header__search" onSubmit={handleSubmit}>
-//         <AiOutlineSearch className="header__icon" />
-//         <input
-//           ref={inputRef}
-//           className="header__input"
-//           type="text"
-//           placeholder="Search for recipes"
-//         />
-
-//         <button className="header__button" type="submit">
-//           Search
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
-// export default Header;
-
-
-
-
 
 
 
